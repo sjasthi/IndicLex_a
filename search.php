66b54bf0ede30d5bd60874606697696757f0e07a
Lucky Vang <bd4000se@go.minnstate.edu>
2026-03-04 20:06:11 -0600

    Update search.php

--- search.php
+++ search.php
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <!-- Placeholder Results Section -->
         <div class="mt-5 text-center text-muted">
-            <h5>Results will appear here!</h5>
+            <h5>Results will appear here</h5>
             <p class="small">Search functionality will be connected to the database later.</p>
         </div>
 
@@ -67,4 +67,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 <?php include 'includes/footer.php'; ?>
+
 
