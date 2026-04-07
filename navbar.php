fa0ae0a7ba0b53aa1780cf1b7bff65fc539c1d93
Rebecca Nowrang <rebecca.nowrang@my.metrostate.edu>
2026-04-06 19:00:26 -0500

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -1,36 +1,55 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$isAdminLoggedIn = isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand fw-bold" href="index.php">IndicLex</a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarNav">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
 
-                <!-- Home -->
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">Home</a>
                 </li>
 
-                <!-- Catalog -->
                 <li class="nav-item">
                     <a class="nav-link" href="catalog.php">Catalog</a>
                 </li>
 
-                <!-- Search -->
+                <li class="nav-item">
+                    <a class="nav-link" href="upload.php">Upload</a>
+                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="search.php">Search</a>
                 </li>
 
-                <!-- Preferences -->
                 <li class="nav-item">
                     <a class="nav-link" href="preferences.php">Preferences</a>
                 </li>
 
-                <!-- Theme Toggle -->
+                <?php if ($isAdminLoggedIn): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin/dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin/logout.php">Logout</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin/login.php">Admin Login</a>
+                    </li>
+                <?php endif; ?>
+
                 <li class="nav-item">
                     <button class="btn btn-outline-light btn-sm ms-3" onclick="toggleTheme()">
                         Toggle Theme
